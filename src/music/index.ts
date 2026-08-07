166320825d91aad991bcb1558fb269b51bee6b01
Sébastien Ferry <sebferry@pm.me>
2026-08-07 09:04:02 +0200

    feat(music): expand CHORD_CATALOG with full 12-root major/minor chord fingerings (#18)

--- src/music/index.ts
+++ src/music/index.ts
@@ -5,6 +5,7 @@
 import { ChordDefinition } from './types';
 
 export const CHORD_CATALOG: ChordDefinition[] = [
+  // C Chords
   {
     name: "C Major",
     key: "C",
@@ -20,47 +21,37 @@ export const CHORD_CATALOG: ChordDefinition[] = [
     ]
   },
   {
-    name: "A Minor",
-    key: "Am",
+    name: "C Minor",
+    key: "Cm",
     suffix: "minor",
     instrument: "guitar-6string",
     fingerings: [
-      { string: 1, fret: 0, text: "E" },
-      { string: 2, fret: 1, text: "C", finger: 1 },
-      { string: 3, fret: 2, text: "A", isRoot: true, finger: 3 },
-      { string: 4, fret: 2, text: "E", finger: 2 },
-      { string: 5, fret: 0, text: "A", isRoot: true },
+      { string: 1, fret: 3, text: "G", finger: 1 },
+      { string: 2, fret: 4, text: "D#", finger: 2 },
+      { string: 3, fret: 5, text: "C", isRoot: true, finger: 4 },
+      { string: 4, fret: 5, text: "G", finger: 3 },
+      { string: 5, fret: 3, text: "C", isRoot: true, finger: 1 },
       { string: 6, fret: -1, text: "X" }
     ]
   },
+
+  // C# / Db
   {
-    name: "G Major",
-    key: "G",
+    name: "C# Major",
+    key: "C#",
     suffix: "major",
     instrument: "guitar-6string",
     fingerings: [
-      { string: 1, fret: 3, text: "G", isRoot: true, finger: 4 },
-      { string: 2, fret: 0, text: "B" },
-      { string: 3, fret: 0, text: "G", isRoot: true },
-      { string: 4, fret: 0, text: "D" },
-      { string: 5, fret: 2, text: "B", finger: 1 },
-      { string: 6, fret: 3, text: "G", isRoot: true, finger: 2 }
+      { string: 1, fret: 4, text: "G#", finger: 1 },
+      { string: 2, fret: 6, text: "F", finger: 4 },
+      { string: 3, fret: 6, text: "C#", isRoot: true, finger: 3 },
+      { string: 4, fret: 6, text: "G#", finger: 2 },
+      { string: 5, fret: 4, text: "C#", isRoot: true, finger: 1 },
+      { string: 6, fret: -1, text: "X" }
     ]
   },
-  {
-    name: "E Major",
-    key: "E",
-    suffix: "major",
-    instrument: "guitar-6string",
-    fingerings: [
-      { string: 1, fret: 0, text: "E", isRoot: true },
-      { string: 2, fret: 0, text: "B" },
-      { string: 3, fret: 1, text: "G#", finger: 1 },
-      { string: 4, fret: 2, text: "E", isRoot: true, finger: 3 },
-      { string: 5, fret: 2, text: "B", finger: 2 },
-      { string: 6, fret: 0, text: "E", isRoot: true }
-    ]
-  },
+
+  // D Chords
   {
     name: "D Major",
     key: "D",
@@ -76,19 +67,81 @@ export const CHORD_CATALOG: ChordDefinition[] = [
     ]
   },
   {
-    name: "B Minor",
-    key: "Bm",
+    name: "D Minor",
+    key: "Dm",
     suffix: "minor",
     instrument: "guitar-6string",
     fingerings: [
-      { string: 1, fret: 2, text: "F#", finger: 1 },
-      { string: 2, fret: 3, text: "D", finger: 2 },
-      { string: 3, fret: 4, text: "B", isRoot: true, finger: 4 },
-      { string: 4, fret: 4, text: "F#", finger: 3 },
-      { string: 5, fret: 2, text: "B", isRoot: true, finger: 1 },
+      { string: 1, fret: 1, text: "F", finger: 1 },
+      { string: 2, fret: 3, text: "D", isRoot: true, finger: 3 },
+      { string: 3, fret: 2, text: "A", finger: 2 },
+      { string: 4, fret: 0, text: "D", isRoot: true },
+      { string: 5, fret: -1, text: "X" },
       { string: 6, fret: -1, text: "X" }
     ]
   },
+
+  // D# / Eb
+  {
+    name: "D# Major",
+    key: "D#",
+    suffix: "major",
+    instrument: "guitar-6string",
+    fingerings: [
+      { string: 1, fret: 3, text: "G", finger: 1 },
+      { string: 2, fret: 4, text: "D#", isRoot: true, finger: 2 },
+      { string: 3, fret: 3, text: "A#", finger: 1 },
+      { string: 4, fret: 5, text: "D#", isRoot: true, finger: 3 },
+      { string: 5, fret: -1, text: "X" },
+      { string: 6, fret: -1, text: "X" }
+    ]
+  },
+
+  // E Chords
+  {
+    name: "E Major",
+    key: "E",
+    suffix: "major",
+    instrument: "guitar-6string",
+    fingerings: [
+      { string: 1, fret: 0, text: "E", isRoot: true },
+      { string: 2, fret: 0, text: "B" },
+      { string: 3, fret: 1, text: "G#", finger: 1 },
+      { string: 4, fret: 2, text: "E", isRoot: true, finger: 3 },
+      { string: 5, fret: 2, text: "B", finger: 2 },
+      { string: 6, fret: 0, text: "E", isRoot: true }
+    ]
+  },
+  {
+    name: "E Minor",
+    key: "Em",
+    suffix: "minor",
+    instrument: "guitar-6string",
+    fingerings: [
+      { string: 1, fret: 0, text: "E", isRoot: true },
+      { string: 2, fret: 0, text: "B" },
+      { string: 3, fret: 0, text: "G" },
+      { string: 4, fret: 2, text: "E", isRoot: true, finger: 3 },
+      { string: 5, fret: 2, text: "B", finger: 2 },
+      { string: 6, fret: 0, text: "E", isRoot: true }
+    ]
+  },
+  {
+    name: "E Dominant 7",
+    key: "E7",
+    suffix: "dom7",
+    instrument: "guitar-6string",
+    fingerings: [
+      { string: 1, fret: 0, text: "E", isRoot: true },
+      { string: 2, fret: 0, text: "B" },
+      { string: 3, fret: 1, text: "G#", finger: 1 },
+      { string: 4, fret: 0, text: "D" },
+      { string: 5, fret: 2, text: "B", finger: 2 },
+      { string: 6, fret: 0, text: "E", isRoot: true }
+    ]
+  },
+
+  // F Chords
   {
     name: "F Major",
     key: "F",
@@ -98,6 +151,20 @@ export const CHORD_CATALOG: ChordDefinition[] = [
       { string: 1, fret: 1, text: "F", isRoot: true, finger: 1 },
       { string: 2, fret: 1, text: "C", finger: 1 },
       { string: 3, fret: 2, text: "A", finger: 2 },
+      { string: 4, fret: 3, text: "F", isRoot: true, finger: 4 },
+      { string: 5, fret: 3, text: "C", finger: 3 },
+      { string: 6, fret: 1, text: "F", isRoot: true, finger: 1 }
+    ]
+  },
+  {
+    name: "F Minor",
+    key: "Fm",
+    suffix: "minor",
+    instrument: "guitar-6string",
+    fingerings: [
+      { string: 1, fret: 1, text: "F", isRoot: true, finger: 1 },
+      { string: 2, fret: 1, text: "G#", finger: 1 },
+      { string: 3, fret: 1, text: "C", finger: 1 },
       { string: 4, fret: 3, text: "F", isRoot: true, finger: 4 },
       { string: 5, fret: 3, text: "C", finger: 3 },
       { string: 6, fret: 1, text: "F", isRoot: true, finger: 1 }
@@ -117,18 +184,110 @@ export const CHORD_CATALOG: ChordDefinition[] = [
       { string: 6, fret: -1, text: "X" }
     ]
   },
+
+  // F# / Gb
   {
-    name: "E Dominant 7",
-    key: "E7",
-    suffix: "dom7",
+    name: "F# Major",
+    key: "F#",
+    suffix: "major",
     instrument: "guitar-6string",
     fingerings: [
-      { string: 1, fret: 0, text: "E", isRoot: true },
+      { string: 1, fret: 2, text: "F#", isRoot: true, finger: 1 },
+      { string: 2, fret: 2, text: "C#", finger: 1 },
+      { string: 3, fret: 3, text: "A#", finger: 2 },
+      { string: 4, fret: 4, text: "F#", isRoot: true, finger: 4 },
+      { string: 5, fret: 4, text: "C#", finger: 3 },
+      { string: 6, fret: 2, text: "F#", isRoot: true, finger: 1 }
+    ]
+  },
+  {
+    name: "F# Minor",
+    key: "F#m",
+    suffix: "minor",
+    instrument: "guitar-6string",
+    fingerings: [
+      { string: 1, fret: 2, text: "F#", isRoot: true, finger: 1 },
+      { string: 2, fret: 2, text: "A", finger: 1 },
+      { string: 3, fret: 2, text: "C#", finger: 1 },
+      { string: 4, fret: 4, text: "F#", isRoot: true, finger: 4 },
+      { string: 5, fret: 4, text: "C#", finger: 3 },
+      { string: 6, fret: 2, text: "F#", isRoot: true, finger: 1 }
+    ]
+  },
+
+  // G Chords
+  {
+    name: "G Major",
+    key: "G",
+    suffix: "major",
+    instrument: "guitar-6string",
+    fingerings: [
+      { string: 1, fret: 3, text: "G", isRoot: true, finger: 4 },
       { string: 2, fret: 0, text: "B" },
-      { string: 3, fret: 1, text: "G#", finger: 1 },
+      { string: 3, fret: 0, text: "G", isRoot: true },
       { string: 4, fret: 0, text: "D" },
-      { string: 5, fret: 2, text: "B", finger: 2 },
-      { string: 6, fret: 0, text: "E", isRoot: true }
+      { string: 5, fret: 2, text: "B", finger: 1 },
+      { string: 6, fret: 3, text: "G", isRoot: true, finger: 2 }
+    ]
+  },
+  {
+    name: "G Minor",
+    key: "Gm",
+    suffix: "minor",
+    instrument: "guitar-6string",
+    fingerings: [
+      { string: 1, fret: 3, text: "G", isRoot: true, finger: 1 },
+      { string: 2, fret: 3, text: "A#", finger: 1 },
+      { string: 3, fret: 3, text: "D", finger: 1 },
+      { string: 4, fret: 5, text: "G", isRoot: true, finger: 4 },
+      { string: 5, fret: 5, text: "D", finger: 3 },
+      { string: 6, fret: 3, text: "G", isRoot: true, finger: 1 }
+    ]
+  },
+
+  // G# / Ab
+  {
+    name: "G# Major",
+    key: "G#",
+    suffix: "major",
+    instrument: "guitar-6string",
+    fingerings: [
+      { string: 1, fret: 4, text: "G#", isRoot: true, finger: 1 },
+      { string: 2, fret: 4, text: "D#", finger: 1 },
+      { string: 3, fret: 5, text: "C", finger: 2 },
+      { string: 4, fret: 6, text: "G#", isRoot: true, finger: 4 },
+      { string: 5, fret: 6, text: "D#", finger: 3 },
+      { string: 6, fret: 4, text: "G#", isRoot: true, finger: 1 }
+    ]
+  },
+
+  // A Chords
+  {
+    name: "A Major",
+    key: "A",
+    suffix: "major",
+    instrument: "guitar-6string",
+    fingerings: [
+      { string: 1, fret: 0, text: "E" },
+      { string: 2, fret: 2, text: "C#", finger: 3 },
+      { string: 3, fret: 2, text: "A", isRoot: true, finger: 2 },
+      { string: 4, fret: 2, text: "E", finger: 1 },
+      { string: 5, fret: 0, text: "A", isRoot: true },
+      { string: 6, fret: -1, text: "X" }
+    ]
+  },
+  {
+    name: "A Minor",
+    key: "Am",
+    suffix: "minor",
+    instrument: "guitar-6string",
+    fingerings: [
+      { string: 1, fret: 0, text: "E" },
+      { string: 2, fret: 1, text: "C", finger: 1 },
+      { string: 3, fret: 2, text: "A", isRoot: true, finger: 3 },
+      { string: 4, fret: 2, text: "E", finger: 2 },
+      { string: 5, fret: 0, text: "A", isRoot: true },
+      { string: 6, fret: -1, text: "X" }
     ]
   },
   {
@@ -142,6 +301,66 @@ export const CHORD_CATALOG: ChordDefinition[] = [
       { string: 3, fret: 0, text: "G" },
       { string: 4, fret: 2, text: "E", finger: 1 },
       { string: 5, fret: 0, text: "A", isRoot: true },
+      { string: 6, fret: -1, text: "X" }
+    ]
+  },
+
+  // A# / Bb
+  {
+    name: "A# Major",
+    key: "A#",
+    suffix: "major",
+    instrument: "guitar-6string",
+    fingerings: [
+      { string: 1, fret: 1, text: "F", finger: 1 },
+      { string: 2, fret: 3, text: "D", finger: 4 },
+      { string: 3, fret: 3, text: "A#", isRoot: true, finger: 3 },
+      { string: 4, fret: 3, text: "F", finger: 2 },
+      { string: 5, fret: 1, text: "A#", isRoot: true, finger: 1 },
+      { string: 6, fret: -1, text: "X" }
+    ]
+  },
+  {
+    name: "A# Minor",
+    key: "A#m",
+    suffix: "minor",
+    instrument: "guitar-6string",
+    fingerings: [
+      { string: 1, fret: 1, text: "F", finger: 1 },
+      { string: 2, fret: 2, text: "C#", finger: 2 },
+      { string: 3, fret: 3, text: "A#", isRoot: true, finger: 4 },
+      { string: 4, fret: 3, text: "F", finger: 3 },
+      { string: 5, fret: 1, text: "A#", isRoot: true, finger: 1 },
+      { string: 6, fret: -1, text: "X" }
+    ]
+  },
+
+  // B Chords
+  {
+    name: "B Major",
+    key: "B",
+    suffix: "major",
+    instrument: "guitar-6string",
+    fingerings: [
+      { string: 1, fret: 2, text: "F#", finger: 1 },
+      { string: 2, fret: 4, text: "D#", finger: 4 },
+      { string: 3, fret: 4, text: "B", isRoot: true, finger: 3 },
+      { string: 4, fret: 4, text: "F#", finger: 2 },
+      { string: 5, fret: 2, text: "B", isRoot: true, finger: 1 },
+      { string: 6, fret: -1, text: "X" }
+    ]
+  },
+  {
+    name: "B Minor",
+    key: "Bm",
+    suffix: "minor",
+    instrument: "guitar-6string",
+    fingerings: [
+      { string: 1, fret: 2, text: "F#", finger: 1 },
+      { string: 2, fret: 3, text: "D", finger: 2 },
+      { string: 3, fret: 4, text: "B", isRoot: true, finger: 4 },
+      { string: 4, fret: 4, text: "F#", finger: 3 },
+      { string: 5, fret: 2, text: "B", isRoot: true, finger: 1 },
       { string: 6, fret: -1, text: "X" }
     ]
   }
